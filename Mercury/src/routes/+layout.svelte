e91ac41ed7d5ff6da10ee22b1b9bf2218e83e532
Vinicius Yahagi Rios <136207802+ViniciusRiosYahagi@users.noreply.github.com>
2025-02-05 15:50:27 +0900

    RR

--- Mercury/src/routes/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-   import '../styles/app.scss'
-</script>
-
-
-<slot></slot>
--- Open_News/src/components/Search.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/components-styles/Search.module.scss'
-
-function Search() {
-  return (
-    <input type="text" className={styles.search} />
-  )
-}
-
-export default Search
