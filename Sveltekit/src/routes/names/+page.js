f2a35256f67faea619adce3105946b892b3d8714
Vinicius Yahagi Rios <136207802+ViniciusRiosYahagi@users.noreply.github.com>
2025-02-27 03:33:10 +0900

    POST
    Criação de um metodo POST dentro do front

--- Sveltekit/src/routes/names/+page.js
+++ Sveltekit/src/routes/names/+page.js
@@ -1,10 +1,6 @@
 export const load = async ({ fetch }) => {
   const response = await fetch('/api/names')
-
-  if(!response.ok) {
-    throw new Error("Erro ao buscar usuarios")
-  }
-  
   const { users } = await response.json()
+  
   return { users }
 }
---
+++ Sveltekit/src/routes/names/add/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+  let name = $state()
+
+  const cadastrarUsuario = async () => {
+    const response = await fetch('/api/names', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name: name})
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+</script>
+
+<div>
+  <label for="name">Nome:</label>
+  <input id="name" bind:value={name} type="text">
+  <button onclick={cadastrarUsuario}>Registrar</button>
+</div>
