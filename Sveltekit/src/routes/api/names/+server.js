fb73cf0e49ee48b0d9e18ba755fefdb63a6aa2c6
Vinicius Yahagi Rios <136207802+ViniciusRiosYahagi@users.noreply.github.com>
2025-02-27 13:10:25 +0900

    Update HTTP Methods

--- Sveltekit/src/routes/api/names/+server.js
+++ Sveltekit/src/routes/api/names/+server.js
@@ -12,19 +12,17 @@ export const GET = async () => {
   try {
 
     const users = getAllUser()
-    
+
     return json(
       { users }, 
       { status: 200 }
     )
-
   } catch (error) {
 
     return json(
       { error: "Erro ao buscar usuarios" }, 
       { status: 500 }
     )
-
   }
 }
 
@@ -35,11 +33,11 @@ export const POST = async ({ request }) => {
   try {  
 
     if (!name) {
+
       return json(
         { error: "Nome é obrigatório"},
         { status: 400}
       )
-
     }
 
     createUser(name);
@@ -48,72 +46,89 @@ export const POST = async ({ request }) => {
       { message: "Nome salvo com sucesso"},
       { status: 201 }
     )
- 
   } catch (error) {
 
     return json(
       { error: "Erro no servidor"},
       { status: 500}
     )
-
   }
 }
 
 export async function PUT({ request }) {
+
+  const { name, id } = await request.json();
+
   try {
-    const { name, id } = await request.json();
+    
 
     if (!id || !name) {
-      new Response(JSON.stringify({ error: "ID e Nome é obrigatorio" }), {
-        status: 400,
-      });
+      
+      return json(
+        { error: "ID e Nome é obrigatório"},
+        { status: 400 }
+      )
     }
 
     const result = changeName(name, id);
 
     if (result.changes > 0) {
-      return new Response(
-        JSON.stringify({ message: "Nome atualizado com sucesso" }),
+
+      return json(
+        { message: "Nome atualizado com sucesso" },
         { status: 200 }
-      );
+      )
     } else {
-      return new Response(JSON.stringify({ error: "Usuário não encontrado" }), {
-        status: 404,
-      });
+
+      return json(
+        { error: "Usuário não encontrado"},
+        { status: 404 }
+      )
     }
   } catch (error) {
-    return new Response(JSON.stringify({ error: "Erro interno no servidor" }), {
-      status: 500,
-    });
+
+    return json(
+      { error: "Erro interno no servidor" },
+      { status: 500 }
+    )
   }
 }
 
 export async function DELETE({ request }) {
+
+  const { id } = await request.json();
+
   try {
-    const { id } = await request.json();
 
     if (!id) {
-      return new Response(JSON.stringify({ error: "ID é obrigatorio" }), {
-        status: 400,
-      });
+
+      return json(
+        {error: "ID é obrigatório" },
+        { status: 400 }
+      )
     }
 
     const result = deleteUser(id);
 
     if (result.changes > 0) {
-      return new Response(JSON.stringify({ message: "Usuário deletado" }), {
-        status: 200,
-      });
+
+      return json(
+        { message: "Usuário deletado"},
+        { status: 200}
+      )
     } else {
-      return new Response(
-        JSON.stringify({ message: "Usuário não encontrado" }),
+
+      return json(
+        { message: "Usuário não encontrado" },
         { status: 404 }
-      );
+      )
     }
   } catch (error) {
-    return new Response(JSON.stringify({ error: "Erro ao deletar" }), {
-      status: 500,
-    });
+
+    return json(
+      { error: "Erro ao deletar" },
+      { status: 500 }
+    )
   }
 }
 
